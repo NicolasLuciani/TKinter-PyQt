fcb322fbe8fa5df7c66a81e6acca06a0473d61be
Nicolas <nicolasluciani1603@gmail.com>
2025-08-12 15:36:37 -0300

    Codigos comentados

--- PyQt_login.py
+++ PyQt_login.py
@@ -2,68 +2,68 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtCore import Qt
 #----------------------------------------------------------------------------------------------
 def login():
-    janela = QWidget() #mudar nome de janela para janela_login
-    janela.resize(310, 350)
-    janela.setStyleSheet("background-color: lightblue;")
-    janela.setWindowTitle("Login")
+    janela_login = QWidget()
+    janela_login.resize(310, 350) # Criar janela Login
+    janela_login.setStyleSheet("background-color: lightblue;")
+    janela_login.setWindowTitle("Login")
     #----------------------------------------------------------------------------------------------
     layout = QVBoxLayout()
 
     titulo = QLabel("LOGIN")
-    titulo.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    titulo.setAlignment(Qt.AlignmentFlag.AlignCenter) # Centralizar o login no meio
     titulo.setStyleSheet("font-size: 24px; font-weight: bold;")
     layout.addWidget(titulo)
     #----------------------------------------------------------------------------------------------
     linha = QFrame()
-    linha.setFrameShape(QFrame.Shape.HLine)
+    linha.setFrameShape(QFrame.Shape.HLine) # Criação de linha azul
     linha.setStyleSheet("color: blue; background-color: blue; height: 2px;")
     layout.addWidget(linha)
     #----------------------------------------------------------------------------------------------
     layout.addSpacing(25)
 
     email = QLineEdit()
-    email.setPlaceholderText("Email")
+    email.setPlaceholderText("Email") # Caixa de texto email
     email.setStyleSheet("background-color: white;")
     layout.addWidget(email)
     #----------------------------------------------------------------------------------------------
     layout.addSpacing(20)
 
     senha = QLineEdit()
-    senha.setPlaceholderText("Senha")
-    senha.setEchoMode(QLineEdit.EchoMode.Password)
+    senha.setPlaceholderText("Senha") # Caixa de texto senha
+    senha.setEchoMode(QLineEdit.EchoMode.Password) # Ocultar a senha
     senha.setStyleSheet("background-color: white;")
     layout.addWidget(senha)
     #----------------------------------------------------------------------------------------------
     layout.addSpacing(120)
 
 
-    def verificar(): # mudar nome de verificar para verificar_login
+    def verificar_login(): # mudar nome de verificar para verificar_login
         if email.text() == "" or senha.text() == "":
-            QMessageBox.warning(janela, "Erro", "O campo não pode estar vazio!")
-        else:
-            janela.close()
+            QMessageBox.warning(janela_login, "Erro", "O campo não pode estar vazio!")
+        else:            # verificar se há campos vazios
+            janela_login.close() # fecha a pagina
             entrada()
 
     botao_login = QPushButton("Entrar")
     botao_login.setStyleSheet("background-color: skyblue;")
-    botao_login.clicked.connect(verificar)
+    botao_login.clicked.connect(verificar_login) # Botao entra que se liga a entrada
     layout.addWidget(botao_login)
 
     botao_cadastro = QPushButton("Não tenho cadastro")
     botao_cadastro.setStyleSheet("background-color: skyblue;")
-    botao_cadastro.clicked.connect(abrir_janela_cadastro)
+    botao_cadastro.clicked.connect(cadastro)
     layout.addWidget(botao_cadastro)
 
-    janelas_abertas.append(janela)
+    janelas_abertas.append(janela_login)
     #----------------------------------------------------------------------------------------------
 
-    janela.setLayout(layout)
-    janela.show()
+    janela_login.setLayout(layout)
+    janela_login.show()
 
 
-def abrir_janela_cadastro():
+def cadastro():
     janela_cadastro = QWidget()
-    janela_cadastro.setWindowTitle("Cadastro")
+    janela_cadastro.setWindowTitle("Cadastro") # Criar janela de cadastro
     janela_cadastro.resize(310, 350)
     janela_cadastro.setStyleSheet("background-color: lightblue;")
 #----------------------------------------------------------------------------------------------
@@ -76,13 +76,13 @@ def abrir_janela_cadastro():
 #----------------------------------------------------------------------------------------------
 
     linha = QFrame()
-    linha.setFrameShape(QFrame.Shape.HLine)
+    linha.setFrameShape(QFrame.Shape.HLine) # linhas azul
     linha.setStyleSheet("color: blue; background-color: blue; height: 2px;")
     layout_cadastro.addWidget(linha)
 #----------------------------------------------------------------------------------------------
     layout_cadastro.addSpacing(20)
 
-    nome_completo = QLineEdit()
+    nome_completo = QLineEdit() # Caixa de texto nome 
     nome_completo.setPlaceholderText("Nome completo")
     nome_completo.setStyleSheet("background-color: white;")
     layout_cadastro.addWidget(nome_completo)
@@ -90,15 +90,15 @@ def abrir_janela_cadastro():
     layout_cadastro.addSpacing(25)
 
     email = QLineEdit()
-    email.setPlaceholderText("Email")
+    email.setPlaceholderText("Email") # Caixa de texto email
     email.setStyleSheet("background-color: white;")
     layout_cadastro.addWidget(email)
 #----------------------------------------------------------------------------------------------
     layout_cadastro.addSpacing(25)
 
     senha = QLineEdit()
-    senha.setPlaceholderText("Senha")
-    senha.setEchoMode(QLineEdit.EchoMode.Password)
+    senha.setPlaceholderText("Senha") # caixa de texto senha
+    senha.setEchoMode(QLineEdit.EchoMode.Password) # ocultar a senha
     senha.setStyleSheet("background-color: white;")
     layout_cadastro.addWidget(senha)
 #----------------------------------------------------------------------------------------------
@@ -106,8 +106,8 @@ def abrir_janela_cadastro():
     def verificar_cadastro():
         if nome_completo.text() == "" or email.text() == "" or senha.text() == "":
             QMessageBox.warning(janela_cadastro, "Erro", "O campo não pode estar vazio!")
-        else:
-            janela_cadastro.close()
+        else:# verificar se há campos vazios
+            janela_cadastro.close()  # fecha a pagina
             login()
     
     layout_cadastro.addSpacing(25)
@@ -126,22 +126,22 @@ def abrir_janela_cadastro():
 #----------------------------------------------------------------------------------------------
 
 def entrada():
-    janela_entrada = QWidget()
-    janela_entrada.setWindowTitle("Você entrou!!!")
-    janela_entrada.resize(310, 350)
-    janela_entrada.setStyleSheet("background-color: lightblue;")
+    janela_inicio = QWidget()
+    janela_inicio.setWindowTitle("Você entrou!!!")
+    janela_inicio.resize(310, 350) # Criar janela de início
+    janela_inicio.setStyleSheet("background-color: lightblue;")
 
-    layout_entrada = QVBoxLayout()
+    layout_entrada = QVBoxLayout() # layout definido de cima para baixo
 #----------------------------------------------------------------------------------------------
     texto = QLabel("Você entrou com sucesso!")
     texto.setAlignment(Qt.AlignmentFlag.AlignCenter)
     texto.setStyleSheet("font-size: 18px; font-weight: bold;")
     layout_entrada.addWidget(texto)
+              # Texto de entrada no início
+    janela_inicio.setLayout(layout_entrada)
+    janela_inicio.show()
 
-    janela_entrada.setLayout(layout_entrada)
-    janela_entrada.show()
-
-    janelas_abertas.append(janela_entrada)
+    janelas_abertas.append(janela_inicio)
 #----------------------------------------------------------------------------------------------
 
 
--- TKinter_login.py
+++ TKinter_login.py
@@ -2,53 +2,53 @@ from tkinter import *
 from tkinter import messagebox
 
 def entrada():
-    Início = Tk()
-    Início.geometry('310x350')
-    Início.title("Início")
-    Início.configure(bg="lightblue")
-    mensagem = Label(Início, text="Bem-vindo à tela inicial!", font=("Arial", 16), bg="Skyblue")
+    janela_inicio = Tk()
+    janela_inicio.geometry('310x350')
+    janela_inicio.title("Início") # Criação da janela login
+    janela_inicio.configure(bg="lightblue")
+    mensagem = Label(janela_inicio, text="Bem-vindo à tela inicial!", font=("Arial", 16), bg="Skyblue")
     mensagem.pack(pady=(50, 0))
-    Início.mainloop()
+    janela_inicio.mainloop()
 #----------------------------------------------------------------------------------------------
 def cadastro():
     janela_cadastrar = Tk()
     janela_cadastrar.geometry('310x350')
-    janela_cadastrar.title("Cadastro")
+    janela_cadastrar.title("Cadastro") #Criação da janela cadastro
     janela_cadastrar.configure(bg="lightblue")
     titulo = Label(janela_cadastrar, text="CADASTRO", bg="lightblue", font=("Arial", 24, "bold"))
     titulo.pack(pady=20)
 #----------------------------------------------------------------------------------------------
-    linha = Frame(janela_cadastrar, height=2, bg="blue")
-    linha.pack(fill="x", padx=20, pady=10)
+    linha_azul = Frame(janela_cadastrar, height=2, bg="blue")
+    linha_azul.pack(fill="x", padx=20, pady=10) # linha azul
 #----------------------------------------------------------------------------------------------
     frame_nome = Frame(janela_cadastrar)
-    frame_nome.pack(fill="x", padx=20)
+    frame_nome.pack(fill="x", padx=20) # frame que contem nome e sobrenome
     nome = Label(frame_nome, text="Nome e sobrenome", font=("Arial", 12, "bold"))
     nome.pack(anchor="w")
 
-    texto_nome = Entry(janela_cadastrar, width=50)
+    texto_nome = Entry(janela_cadastrar, width=50) # caixa de texto nome e sobrenome
     texto_nome.pack(padx=20)
 #----------------------------------------------------------------------------------------------
     frame_email = Frame(janela_cadastrar)
-    frame_email.pack(fill="x", padx=20, pady=(20, 0))
+    frame_email.pack(fill="x", padx=20, pady=(20, 0)) #frame que contem email
     email = Label(frame_email, text="Email", font=("Arial", 12, "bold"))
     email.pack(anchor="w")
 
-    texto_email = Entry(janela_cadastrar, width=50)
+    texto_email = Entry(janela_cadastrar, width=50)# caixa de texto email
     texto_email.pack(padx=20)
 #----------------------------------------------------------------------------------------------
     frame_senha = Frame(janela_cadastrar)
-    frame_senha.pack(fill="x", padx=20, pady=(20, 0))
+    frame_senha.pack(fill="x", padx=20, pady=(20, 0))# frame que contem senha
     senha = Label(frame_senha, text="Senha", font=("Arial", 12, "bold"))
     senha.pack(anchor="w")
 
-    texto_senha = Entry(janela_cadastrar, width=50, show="*")
+    texto_senha = Entry(janela_cadastrar, width=50, show="*") # caixa de texto senha que oculta os caracteres
     texto_senha.pack(padx=20, anchor="w")
 #----------------------------------------------------------------------------------------------
     def verificar_cadastro():
         nome = texto_nome.get()
         email = texto_email.get()
-        senha = texto_senha.get()      
+        senha = texto_senha.get()      # verificar se há algum campo vazio
         if nome == "" or email == "" or senha == "":
             messagebox.showerror("Erro", "Preencha todos os campos!")
         else:
@@ -56,53 +56,53 @@ def cadastro():
             login()
 #----------------------------------------------------------------------------------------------
     login_botao = Button(janela_cadastrar, text= "Login", bg = "skyblue", command=verificar_cadastro)
-    login_botao.pack(padx= 20, pady=(20, 0))
-    
+    login_botao.pack(padx= 20, pady=(20, 0))# botao login
+     
     janela_cadastrar.mainloop()
 #----------------------------------------------------------------------------------------------
 def login():
-    janela = Tk() #mudar nome de janela para janela_login
-    janela.title("login")
-    janela.geometry('310x350')
-    janela.configure(bg="lightblue")
+    janela_login = Tk() #mudar nome de janela para janela_login
+    janela_login.title("login")
+    janela_login.geometry('310x350')
+    janela_login.configure(bg="lightblue")
 #----------------------------------------------------------------------------------------------
-    titulo = Label(janela, text="LOGIN", bg="lightblue", font=("Arial", 24, "bold"))
-    titulo.pack(pady=20)
+    titulo = Label(janela_login, text="LOGIN", bg="lightblue", font=("Arial", 24, "bold"))
+    titulo.pack(pady=20) # titulo login
 #----------------------------------------------------------------------------------------------
-    linha = Frame(janela, height=2, bd=0, bg="blue")
+    linha = Frame(janela_login, height=2, bd=0, bg="blue") # linha azul
     linha.pack(fill="x", padx=20, pady=10)
 #----------------------------------------------------------------------------------------------
-    frame_email = Frame(janela)
-    frame_email.pack(fill="x", padx=20)
+    frame_email = Frame(janela_login)
+    frame_email.pack(fill="x", padx=20) # frame que contem email
     email = Label(frame_email, text="Email", font=("Arial", 12, "bold"))
     email.pack(anchor="w")
 
-    texto_email = Entry(janela, width=50)
-    texto_email.pack(padx=20, anchor="w")
+    texto_email = Entry(janela_login, width=50)
+    texto_email.pack(padx=20, anchor="w")# caixa de texto email
 #----------------------------------------------------------------------------------------------
-    frame_senha = Frame(janela)
-    frame_senha.pack(fill="x", padx=20, pady=(45, 0))
+    frame_senha = Frame(janela_login)
+    frame_senha.pack(fill="x", padx=20, pady=(45, 0))# frame que contem senha
     senha = Label(frame_senha, text="Senha", font=("Arial", 12, "bold"))
     senha.pack(anchor="w")
 
-    texto_senha = Entry(janela, width=50, show="*")
+    texto_senha = Entry(janela_login, width=50, show="*")# caixa de texto senha que oculta os caracteres
     texto_senha.pack(padx=20, anchor="w")
 #----------------------------------------------------------------------------------------------
     def verificar_login():
         email = texto_email.get()
         senha = texto_senha.get()
-        if email == "" or senha == "":
+        if email == "" or senha == "":   # verificar se há algum campo vazio
             messagebox.showerror("Erro", "Preencha todos os campos!")
         else:
-            janela.destroy()
+            janela_login.destroy()
             entrada()
 #----------------------------------------------------------------------------------------------
-    entrar = Button(janela, text="Entrar", bg="skyblue", command=verificar_login)
+    entrar = Button(janela_login, text="Entrar", bg="skyblue", command=verificar_login)
     entrar.pack(padx=20, pady=(30, 0))
 #----------------------------------------------------------------------------------------------
-    sem_cadastro = Button(janela, text="Não tenho cadastro", bg="blue", font=("Arial", 12), fg="skyblue", command=cadastro)
+    sem_cadastro = Button(janela_login, text="Não tenho cadastro", bg="blue", font=("Arial", 12), fg="skyblue", command=cadastro)
     sem_cadastro.pack(padx=20, pady=(12, 0))
 #----------------------------------------------------------------------------------------------
-    janela.mainloop()
+    janela_login.mainloop()
 
 login()
